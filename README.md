0897d2e7c5b166590dba279382f1470724af1f3d
Ivan Leschinsky <vanopiano@gmail.com>
2025-02-12 22:39:28 +0300

    Add separate solana monitoring

--- README.md
+++ README.md
@@ -3,5 +3,6 @@
 
 Usage:
 ```bash
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ivan-leschinsky/solana-configs/v2.2/install_solana_metrics.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ivan-leschinsky/solana-configs/v2.3/install_solana_metrics.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/ivan-leschinsky/solana-configs/v2.3/install_solana_monitoring.sh)"
 ```
--- install_solana_monitoring.sh
+++ install_solana_monitoring.sh
@@ -6,63 +6,18 @@ mkdir -p $SOLANA_DIR
 
 echo "###################### WARNING!!! ###################################"
 echo "###   This script will perform the following operations:          ###"
-echo "###   * system tuning (option) (install systuner.service)         ###"
-echo "###   * system tuning (edit fstrim.timer daily on mainnet)        ###"
 echo "###   * install monitoring (option)                               ###"
-echo "###   * configure firewall                                        ###"
 echo "###                                                               ###"
-echo "###   *** Script provided by vano.one (originally MARGUS.ONE      ###"
+echo "###   *** Script provided by vano.one (originally MARGUS.ONE)     ###"
 echo "#####################################################################"
 echo
 
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 echo "#############################################################"
-echo "###       Installing base:   curl, wget, git, gnupg       ###"
+echo "###       Installing base:   curl, wget       ###"
 echo "#############################################################"
-apt update -y && apt upgrade -y && apt install curl gnupg wget -y
-
-
-system_tuning() {
-
-echo "#####################################################"
-echo "###       Start system tuning by MARGUS.ONE       ###"
-echo "#####################################################"
-
-curl -fsSL https://api.margus.one/solana/tuning.sh | bash
-
-select cluster in "mainnet-beta" "testnet"; do
-  case $cluster in
-      mainnet-beta )
-    		clusternetwork="mainnet"
-        break;;
-      testnet )
-    		clusternetwork="testnet"
-        break;;
-  esac
-done
-
-if [[ $clusternetwork = testnet ]];then
-echo "This is testnet. No edit fstrim.timer"
-elif [ $clusternetwork = mainnet ];then
-echo "Edit /lib/systemd/system/fstrim.timer"
-cat > /lib/systemd/system/fstrim.timer <<EOF
-[Unit]
-Description=Discard unused blocks once a week
-Documentation=man:fstrim
-ConditionVirtualization=!container
-
-[Timer]
-OnCalendar=daily
-AccuracySec=1h
-Persistent=true
-
-[Install]
-WantedBy=timers.target
-EOF
-fi
-}
-
+apt update -y && apt upgrade -y && apt install curl wget -y
 
 
 install_monitoring() {
@@ -131,63 +86,7 @@ EOF
 sudo systemctl enable --now telegraf
 }
 
-ufw_configure() {
-echo "###########################################"
-echo "###      Configure ufw firewall         ###"
-echo "###      Enable ufw (y|n)?              ###"
-echo "###########################################"
-apt update -y && apt upgrade -y && apt install ufw -y
-sudo ufw allow 22/tcp
-sudo ufw allow 2222/tcp
-sudo ufw allow 8000:8020/tcp
-sudo ufw allow 8000:8020/udp
-sudo ufw allow 8899:8900/tcp
-ufw allow 80:81/tcp
-sudo ufw deny out from any to 10.0.0.0/8
-sudo ufw deny out from any to 172.16.0.0/12
-sudo ufw deny out from any to 192.168.0.0/16
-sudo ufw deny out from any to 100.64.0.0/10
-sudo ufw deny out from any to 198.18.0.0/15
-sudo ufw deny out from any to 169.254.0.0/16
-sudo ufw enable
-}
-
-echo "### Add System tuning?  Enter 1 or 2  ###"
-select systemtuning in "Yes" "No"; do
-    case $systemtuning in
-        Yes )
-        system_tuning
-          break;;
-        No )
-          break;;
-    esac
-done
-
-
-echo "### Install firewal?   Enter 1 or 2  ###"
-select firewall in "Yes" "No"; do
-  case $firewall in
-        Yes )
-        ufw_configure
-          break;;
-        No )
-          break;;
-    esac
-done
-
-
-
-echo "### Install monitoring?  Enter 1 or 2  ###"
-select monitoring in "Yes" "No"; do
-    case $monitoring in
-        Yes )
-          install_monitoring
-          break;;
-        No )
-          break;;
-    esac
-done
-
+install_monitoring
 echo "### Done."
 
 exit 0
